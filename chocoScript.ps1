f07f16d7771bcf0ccd56d5e299551f2cc0784c1b
Sav Talati <143217839+utsavhtalati@users.noreply.github.com>
2024-01-04 13:36:21 -0600

    Update chocoScript.ps1

--- chocoScript.ps1
+++ chocoScript.ps1
@@ -1,4 +1,3 @@
-iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install office365business -y
 choco install nodejs -y
 choco install openjdk -y
