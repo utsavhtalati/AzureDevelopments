043d998fedc5248d165ad895cb58ff0d69a6d57e
Sav Talati <143217839+utsavhtalati@users.noreply.github.com>
2024-01-04 13:29:31 -0600

    Update chocoScript.ps1

--- chocoScript.ps1
+++ chocoScript.ps1
@@ -1,5 +1,4 @@
-Set-ExecutionPolicy AllSigned
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install office365business
 choco install nodejs 
 choco install openjdk
