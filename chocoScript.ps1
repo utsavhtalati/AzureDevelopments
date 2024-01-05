5412a96476c796ceaead74e1e161df10d277e7bf
Sav Talati <143217839+utsavhtalati@users.noreply.github.com>
2024-01-05 12:54:49 -0600

    Update chocoScript.ps1

--- chocoScript.ps1
+++ chocoScript.ps1
@@ -1,1 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+choco install git -y
+choco install nodejs-lts -y
+choco install vscode -y
+choco install office365business -y
+choco install javaruntime -y
